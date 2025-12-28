cafd1d09134852250b9a196eaf8019738f6c66fc
ff34d <urajimirudev@gmail.com>
2025-12-28 15:53:50 +0400

    fix: type error

--- src/modules/Ast/parsers/parseFlow.ts
+++ src/modules/Ast/parsers/parseFlow.ts
@@ -2,13 +2,13 @@ import { NodeType, TokenType } from "../../../main"
 import type { ASTParser } from "../../../types/Ast"
 import { parsePointer } from "../utils/parsePointer"
 
-export const parseFlow: ASTParser = (ctx, L, S) => {
+export const parseFlow: ASTParser = (ctx) => {
    const pointerToken = ctx.expect(TokenType.pointer, "Expected flow token")
 
    const firstId = ctx.peek(-2)
    const lastId = ctx.peek(0)
 
-   const pointerData = parsePointer(pointerToken.value, L, S)
+   const pointerData = parsePointer(pointerToken.value)
 
    return {
       type: NodeType.FLOW,
